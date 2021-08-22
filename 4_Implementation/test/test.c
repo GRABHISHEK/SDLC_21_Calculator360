52b27a869c232e838c27c5e6b81f43ff9c296e99
GRABHISHEK <86198896+GRABHISHEK@users.noreply.github.com>
2021-08-23 00:34:08 +0530

    update on electronics testcase

--- 4_Implementation/test/test.c
+++ 4_Implementation/test/test.c
@@ -1,6 +1,11 @@
 #include "unity.h"
 #include "calculator.h"
 #include "electricalcalc.h"
+#include "Electronics.h"
+#include "unity_internals.h"
+#include "../inc/Electronics.h"
+#include "../unity/unity.h"
+#include "../unity/unity_internals.h"
 #include <math.h>
 #define PROJECT_NAME "Math Calculator"
 static arthimetic_inputs a_i={0,0,0};
@@ -410,4 +415,65 @@ void testohmR(void)
   float I = 6.5, V = 2.1;
   TEST_ASSERT_EQUAL_FLOAT(3.1, ohmI(I, V));
   TEST_ASSERT_NOT_EQUAL(4.2, ohmI(I, V));
+}
+/**
+ * @file test.c
+ * @author Abhishek G R
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * @brief test functions 
+ * 
+ */
+
+void setUp(){}
+void tearDown(){}
+
+/**
+ * @brief main function of test.c 
+ * 
+ */
+void cosine_transform(){
+  TEST_ASSERT_EQUAL_INT(1, cosinetransform(111,123));
+  TEST_ASSERT_EQUAL_INT(0, cosinetransform(-777,123));
+
+}
+void sine_transform(){
+  TEST_ASSERT_EQUAL_INT(1, sinetransform(9837));
+  TEST_ASSERT_EQUAL_INT(0, sinetransform(0));
+
+}
+void Hadmard_transform(){
+  TEST_ASSERT_EQUAL_INT(1, Hadmardtransform(9837));
+  TEST_ASSERT_EQUAL_INT(0, Hadmardtransform(4545));
+  
+}
+void  Discrete_Fourier_Transform(){
+  TEST_ASSERT_EQUAL_INT(1, DiscreteFourierTransform(9837,99));
+  TEST_ASSERT_EQUAL_INT(0, DiscreteFourierTransform(-8080,23));
+
+}
+void Runlength_encoding_compression(){
+  TEST_ASSERT_EQUAL_INT(1, RunlengthEncodingCompression(111,123));
+  TEST_ASSERT_EQUAL_INT(0, RunlengthEncodingCompression(000,69));
+
+}
+int main()
+{
+  /* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+  RUN_TEST(cosine_transform);
+  RUN_TEST(sine_transform);
+  RUN_TEST(Hadmard_transform);
+  RUN_TEST(Discrete_Fourier_Transform);
+  RUN_TEST(Runlength_encoding_compression);
+
+  
+  return UNITY_END();
 }
