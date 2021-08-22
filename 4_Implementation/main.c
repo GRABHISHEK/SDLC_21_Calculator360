252d9d254fbd2427e5f25b5ef2adafb1f633466d
Lok Prakash Raju Goli <35807847+LPR1234@users.noreply.github.com>
2021-08-22 23:50:31 +0530

    Add files via upload

--- 4_Implementation/main.c
+++ 4_Implementation/main.c
@@ -1,9 +1,11 @@
 #include "electricalcalc.h"
 #include "Electronics.h"
 #include "calculator.h"
+#include"fin.h"
+
 int main()
 {
-    printf("Choose your option \n 1.Electrical \n 2.Electronic \n 3.Arithmetic \n");
+    printf("Choose your option \n1.Electrical \n2.Electronic \n3.Arithmetic \n4. \n5.Finance \n6. \n");
     int choice;
     scanf("%d", &choice);
     switch (choice)
@@ -11,12 +13,21 @@ int main()
     case 1:
         electricalcalc();
         break;
-     case 2:
+    case 2:
         Electronicsfun();
         break;   
-     case 3:
+    case 3:
         mathcalc();
         break;
+    case 4:
+        break;
+    case 5:
+        financecal();
+        break;
+    case 6:
+        break;
+    default:
+        printf("\nInvalid choice, please choose from 1 to 6");
     }
     return 0;
 }
