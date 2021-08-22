c0006e719f8cf50e002b0e6290e40e0d99aaa97b
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-23 00:03:43 +0530

    Add files via upload

---
+++ 4_Implementation/src/Physics/friction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "physicscalc.h"
+
+void friction()
+{
+    int k;
+    float F,N,sf; // variable declaration.
+    float g=9.8 // in m/s^2
+
+    printf("Enter the value of F and then N");
+    scanf("%f %f ",&r,&p); // input statement which take the value.
+    sf=F/N; // static friction
+    printf("%f",sf);
+}
