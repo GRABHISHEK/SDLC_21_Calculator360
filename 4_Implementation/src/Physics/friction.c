281830691106e66e90a2043df67f168a5aebeb5f
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-23 15:26:50 +0530

    Update friction.c

--- 4_Implementation/src/Physics/friction.c
+++ 4_Implementation/src/Physics/friction.c
@@ -8,7 +8,7 @@ void friction()
     float g=9.8; // in m/s^2
 
     printf("Enter the value of F and then N");
-    scanf("%f %f ",&r,&p); // input statement which take the value.
+    scanf("%f %f ",&F,&N); // input statement which take the value.
     sf=F/N; // static friction
     printf("%f",sf);
 }
