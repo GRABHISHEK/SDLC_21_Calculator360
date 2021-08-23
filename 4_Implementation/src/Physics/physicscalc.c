9a69ac019965f715133858efef65198729847927
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-23 23:08:51 +0530

    Add files via upload

---
+++ 4_Implementation/src/Physics/physicscalc.c
@@ -0,0 +1,68 @@
+#include "physicscalc.h"
+
+int physicscalc()
+{
+    printf("\n 1. Newtonian Gravity\n 2. Newtons\n 3. amTorque\n 4. circularMotion\n 5.display_velocity\n 6. friction\n 7. light\n 8. mfi\n 9. moi\n 10. reducedMass\n 11. sound\n 12. work_energy\n 13.heightOfProjectile\n 14. conicalPendulum\n 15. simplePendulum\n 16.lenslaw\n");
+    int c;
+    printf("\nEnter your choice : ");
+    scanf("%d", &c);
+    switch (c)
+    {
+    case 1:
+        force();
+        break;
+    case 2:
+        Newtons();
+        break;
+    case 3:
+        AmTorque();
+        break;
+    case 4:
+        circularMotion();
+        break;
+    case 5:
+        disVelocity();
+        break;
+    case 6:
+        friction();
+        break;
+    case 7:
+    {
+        LightFormulas();
+        break;
+    }
+    case 8:
+    {
+        mofoim();
+        break;
+    }
+    case 9:
+        moi();
+        break;
+
+    case 10:
+        reduceMass();
+        break;
+    case 11:
+        SoundFormulas();
+        break;
+    case 12:
+        workEnergypower();
+        break;
+    case 13:
+        heightOfPendulum();
+        break;
+    case 14:
+        conicalPendulum();
+        break;
+    case 15:
+        simplePendulum();
+        break;
+    case 16:
+        lenslaw();
+        break;
+    default:
+        printf("\nWrong option");
+        physicscalc();
+    }
+}
