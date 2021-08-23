7bacfffd37b9ef2a5f9d2b1f03690f637bb83de4
Triveni22 <85540441+Triveni22@users.noreply.github.com>
2021-08-23 15:15:06 +0530

    Add files via upload

---
+++ 4_Implementation/src/Physics/lenslaw.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void lenslaw() {
+     float u,f;
+    scanf("%f %f",&u,&f);
+    float v;
+   float U=1/u;
+   float F=1/f;
+   float V=F-U;
+    printf("Lens Law 1/v is %0.6f", V);
+}
