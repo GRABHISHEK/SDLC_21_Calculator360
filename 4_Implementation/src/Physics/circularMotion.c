65f9f76e6870464c34e036ee226caa9c86b236c7
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-23 15:16:48 +0530

    Update circularMotion.c

--- 4_Implementation/src/Physics/circularMotion.c
+++ 4_Implementation/src/Physics/circularMotion.c
@@ -5,17 +5,16 @@ void circularMotion()
 {
   int k;
   float m,v,r,fc,ca; // variable declaration.
+  printf("Enter the values of m, v and then r");
   scanf("%f %f %f",&m,&v,&r);
 
   printf("Choose the required output 1.centripetal force 2.centripetal acceleration"); // instruction for the user.
   scanf("%d", &k);
   if(k==1){
-      printf("Enter the value of r and then p");
       fc=(m*v*v)/r; // centripetal force
       printf("%f",fc);
   }
   else if (k==2) {
-      printf("Enter the value of r and then f");
       ca=(v*v)/r; // centripetal acceleration
       printf("%f", ca);
   }
