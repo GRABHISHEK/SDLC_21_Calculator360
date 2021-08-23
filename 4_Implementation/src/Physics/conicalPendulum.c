3bf9769213cc972b2518a4d0b441a643335b5e41
Triveni22 <85540441+Triveni22@users.noreply.github.com>
2021-08-23 18:45:04 +0530

    Add files via upload

---
+++ 4_Implementation/src/Physics/conicalPendulum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <math.h>
+void conicalPendulum() {
+ double g=2;
+ double L=4;
+  double teta=30;
+  double up=sqrt((L*cos(teta))/g);
+ double T=2*3.14*up;
+ printf("Conical Pendulum %.2lf\n",T);
+}
