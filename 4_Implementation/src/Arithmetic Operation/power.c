cfcebc69c512582d1bcc85c3c2ec46a1f8904b9c
302471 <86583259+302471@users.noreply.github.com>
2021-08-23 12:42:26 +0530

    Update power.c

--- 4_Implementation/src/Arithmetic Operation/power.c
+++ 4_Implementation/src/Arithmetic Operation/power.c
@@ -1,7 +1,7 @@
 #include "calculator.h"
 #include <stdio.h>
 #include <math.h>
-error_t power(arthimetic_inputs *ai){
+error_t power(arithmetic_inputs *ai){
     ai->output=pow((ai->input_1),(ai->input_2));
     return SUCCESS;
 }
