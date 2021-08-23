466c10567db10c16da068b069b3deb5575bc83f1
302471 <86583259+302471@users.noreply.github.com>
2021-08-23 19:54:16 +0530

    Update factorial.c

--- 4_Implementation/src/ArithmeticOperations/factorial.c
+++ 4_Implementation/src/ArithmeticOperations/factorial.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-error_t factorial(single_inputs *si){
-    float number_1=si->input_3;
+error_t factorial(other_inputs *si){
+    long long number_1=si->input_5;
     
     if(number_1<=0){
-	si->output_2=0;
+	oi->output_4=0;
         return FAILURE;
     }
-    si->output_2=1;
+    oi->output_4=1;
     while(number_1>0){
-        si->output_2=si->output_2 * number_1;
+        si->output_4=si->output_4 * number_1;
         number_1--;
     }
     return SUCCESS;
