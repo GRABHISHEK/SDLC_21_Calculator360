47fca5bb51ed6d6a2a215063de058a3a0ead7c2d
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-23 23:24:13 +0530

    Update physicscalc.h

--- 4_Implementation/inc/physicscalc.h
+++ 4_Implementation/inc/physicscalc.h
@@ -5,26 +5,25 @@
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14
+#define g 9.8
 
 int physicscalc();
-void Newtons();
-void AmTorque(); // Angular momentum and torque
-void reducedMass(); // Reduced mass
 void force(); // NewtonianGravity
+void Newtons(); // Newton
+void AmTorque(); // Angular momentum and torque
 void circularMotion(); // centripetal force and centripetal acceleration
 void disVelocity(); // displacement, final velocity, average velocity
 void friction(); // static friction
+void LightFormulas(); //light
 void mofoim(); //momentum,force,impulse,pressure,density
 void moi(); // moment of inertia
-void reduceMass(); // reduced mass
-void workEnergypower(); // work,potential energy,kinetic energy,power
+void reducedMass(); // Reduced mass
 void SoundFormulas(); // sound
-void LightFormulas(); //light
-void lenslaw(); //lenslaw
-void simplePendulum(); // simplePendulum
+void workEnergypower(); // work,potential energy,kinetic energy,power
+void heightOfPendulum(); // heightOfPendulum
 void conicalPendulum(); // conicalPendulum
-void heightOfProjectile(); // heightOfPendulum
-
+void simplePendulum(); // simplePendulum
+void lenslaw(); //lenslaw
 
 
 #endif
