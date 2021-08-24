0d0b3a8c62659d14aa997fbc72d2c82dee582a66
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-24 18:01:19 +0530

    Update work_energy.c

--- 4_Implementation/src/Physics/work_energy.c
+++ 4_Implementation/src/Physics/work_energy.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include "physicscalc.h"
 
-void workEnergypower() // main function declaration
+float workEnergypower(int k,float m,float F,float D,float h,float v,float V,float R) // main function declaration
 {
-  int k;
-  float F,D,W,m,PE,KE,h,v,P,R,V; // variable declaration
-  float g = 9.8;
-  printf("Enter the value of m");
-  scanf("%f",&m);
-  printf("Choose the required output 1.Work 2.Energy 3.Power"); // instruction for the user.
-  scanf("%d", &k);
+  
+  float W,PE,KE,P;
+  float g=9.8;
   if(k==1){
-      printf("Enter the value of F and then D"); // instruction for the user.
-      scanf("%f %f",&F,&D);
       W=F*D; // Work
       printf("%f",W);
+      return W;
   }
   else if (k==2) {
-      printf("Enter the value of h and then v"); // instruction for the user.
-      scanf("%f %f",&h,&v);
       PE=m*g*h; // potential energy
       KE=0.5*(m*v*v);// kinetic energy
       printf("%f %f", PE,KE);
+      return PE;
   }
   else if(k==3){
-      printf("Enter the value of V and R"); // instruction for the user.
-      scanf("%f %f", &V,&R);
       P=(V*V)/R; // Power
       printf("%f",P);
+      return P;
   }
   else{
       printf("No option selected");
+      return 0;
   }
 }
