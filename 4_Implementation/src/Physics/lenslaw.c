db400e3a399a857045e72c6478fdda9b05395fdd
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-24 17:39:19 +0530

    Update lenslaw.c

--- 4_Implementation/src/Physics/lenslaw.c
+++ 4_Implementation/src/Physics/lenslaw.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 #include "physicscalc.h"
-void lenslaw() {
-     float u,f;
-    scanf("%f %f",&u,&f);
-    float v;
+void lenslaw(float u, float f) {
    float U=1/u;
    float F=1/f;
    float V=F-U;
     printf("Lens Law 1/v is %0.6f", V);
+    return V;
 }
