ed3d7ae731b08520ef6eff289e50d7ccd582285e
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-24 17:30:53 +0530

    Update Newtons.c

--- 4_Implementation/src/Physics/Newtons.c
+++ 4_Implementation/src/Physics/Newtons.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "physicscalc.h"
-void Newtons()
+float Newtons(float mass, float acceleration)
 {
-    int force,mass,acceleration;
-    printf("\n\t enter the mass of a body = ");
-    scanf("%d",&mass);
-    printf("\n\t enter the acceleration of a body = ");
-    scanf("%d",&acceleration);
     force=mass*acceleration;
     printf("\n\t the force acting on a bode = %d",force);
+    return force;
 }
