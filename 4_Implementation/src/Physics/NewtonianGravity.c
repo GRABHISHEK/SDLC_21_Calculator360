3b67228c63065310a79ae47cf787c9241f95fd91
uday-3773 <86252404+uday-3773@users.noreply.github.com>
2021-08-24 14:31:09 +0530

    Update NewtonianGravity.c

--- 4_Implementation/src/Physics/NewtonianGravity.c
+++ 4_Implementation/src/Physics/NewtonianGravity.c
@@ -6,8 +6,11 @@ void force()
     float G;
     G = 6.67 / 1e11;
     float F,m1,m2,r;
+    printf("Enter the values of m1, m2 and r");
     scanf("%f %f %f",&m1,&m2,&r);
     F = (G * m1 * m2) / (r * r);
     float value = (int)(F * 100 + .5);
-    printf("%f",(float)value / 100);
+    value = (float)value/100;
+    printf("%f",value);
+    return value;
 }
